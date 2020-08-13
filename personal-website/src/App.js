6ab8603c665f971e46c5e07beeb948099f782b23
dbzheng <dbzheng@umich.edu>
2020-08-13 18:22:27 -0400

    added footer. Changed to use flexbox

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Intro from "./Intro";
 import Background from "./Background";
+import Footer from "./Footer";
 class App extends React.Component {
     render() {
         return (
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Intro />
                 <Background />
+                <Footer />
             </div>
         );
     }
