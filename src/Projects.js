1d48b3fdec10d857b8988bab358856821b0ac5d7
Daniel Zheng <danielzheng@Daniels-MacBook-Pro-3.local>
2020-08-27 22:54:49 -0400

    uh

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import ProjectCard from "./ProjectCard";
 export class Projects extends Component {
     render() {
@@ -8,9 +8,9 @@ export class Projects extends Component {
                 <Grid item container>
                     <Grid xs={0} sm={2} />
                     <Grid item xs={12} sm={8}>
-                        <Typography variant="h6" style={{ marginTop: "50px" }}>
+                        <h6>
                             Advisors
-                        </Typography>
+                        </h6>
                         <Grid container spacing={4}>
                             <ProjectCard />
                         </Grid>
