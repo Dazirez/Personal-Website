1c088cc354b0bd9d30eedbfd57b038b2dabd0d79
Daniel Zheng <dbzheng@umich.edu>
2021-06-04 02:20:13 -0400

    added socials

--- src/Intro.js
+++ src/Intro.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 const Text = styled.body`
   font-family: Apercu, 'Source Sans Pro', system, system-ui, -apple-system,
@@ -30,7 +31,11 @@ export class Intro extends Component {
               <h1>
                 Daniel <span></span>
               </h1>
-             
+              <SocialIcon network="github" url="https://github.com/Dazirez" style={{ marginTop: 25}}/>
+              <SocialIcon network="linkedin" url="https://www.linkedin.com/in/dbzheng/" style={{ marginLeft: 25, marginTop: 25}}/>
+              <SocialIcon network="youtube" url="https://www.youtube.com/channel/UCcCYKEueIGUsdsnWm1_ZY8g" style={{ marginLeft: 25, marginTop: 25}}/>
+              <SocialIcon network="twitch" url="https://www.twitch.tv/loldreamer/" style={{ marginLeft: 25, marginTop: 25}}/>
+
             </div>
             <Pic src={require('./assets/img/propic-2.jpeg')} />
           </div>
