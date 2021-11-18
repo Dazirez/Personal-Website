f0814853c3db905208866db4807400cae85dc14f
Daniel Zheng <dbzheng@umich.edu>
2021-11-17 22:14:08 -0500

    updates

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -26,20 +26,13 @@ const ProjectCard = (props) => {
   const { title, link, imgSrc } = props;
   return (
     <div>
-      <Card>
+      <Card style={{width: '15vw'}}>
         <CardActionArea className={classes.img}>
           <CardMedia style={{ height: '150px' }} image={imgSrc} title={title} />
         </CardActionArea>
       </Card>
       <br></br>
-      <Button className={classes.button} variant='contained' color={primary}>
-        {title}
-      </Button>
-      <Button variant='contained' color='secondary'>
-        <Link href={link} target='none'>
-          Github
-        </Link>
-      </Button>
+   
     </div>
   );
 };
